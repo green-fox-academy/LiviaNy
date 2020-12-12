7686e24e28351972687d1cfac80d90aab4fc82a6
LiviaNy <livia.nyitrai@gmail.com>
2020-12-12 18:45:38 +0100

    datastruct

---
+++ Week-3/day-3/data-structures/List-intro1.ts
@@ -0,0 +1,6 @@
+let list: string[] = []
+console.log(list.length);
+list.push(`William`);
+console.log(list);
+list.push(`John`);
+console.log(list)
---
+++ Week-3/day-3/takes-longer.ts
@@ -0,0 +1,13 @@
+'use strict';
+// While saving this quote, a disk error has occured. Please fix it.
+// Add "always takes longer than" between the words "It" and "you"
+
+let quote: string = `Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.`;
+
+let quote1 = quote.split(`It`);
+quote1.splice(1, 0, `always takes longer than`);
+quote = quote1.join(`It `)
+
+
+console.log(quote);
+
