901c095a725dd812c5f2618725407d13134f2b12
LiviaNy <livia.nyitrai@gmail.com>
2020-12-13 22:33:16 +0100

    finished-sunday

--- Week-3/day-3/data-structures/Map-intro2.ts
+++ Week-3/day-3/data-structures/Map-intro2.ts
@@ -7,13 +7,13 @@ let map2: object = {
     "978-1-60309-461-0": `The Lab`
 }
 //Print all the key-value pairs in the following format
-    // A Letter to Jo (ISBN: 978-1-60309-452-8)
-    // Lupus (ISBN: 978-1-60309-459-7)
-    // Red Panda and Moon Bear (ISBN: 978-1-60309-444-3)
-    // The Lab (ISBN: 978-1-60309-461-0)
-for (const [key, value] of Object.entries(map2)){
-        console.log(`${value}` + ` (ISMB:` + `${key}` + `)`);
-    }
+// A Letter to Jo (ISBN: 978-1-60309-452-8)
+// Lupus (ISBN: 978-1-60309-459-7)
+// Red Panda and Moon Bear (ISBN: 978-1-60309-444-3)
+// The Lab (ISBN: 978-1-60309-461-0)
+for (const [key, value] of Object.entries(map2)) {
+    console.log(`${value}` + ` (ISMB:` + `${key}` + `)`);
+}
 //Remove the key-value pair with key 978-1-60309-444-3
 delete map2["978-1-60309-444-3"];
 //Remove the key-value pair with value The Lab
@@ -26,23 +26,23 @@ console.log(map2);
 map2["978-60309-450-4"] = `They Called Us Enemy`;
 map2["978-60309-453-5"] = `Why Did We Trust Him?`;
 //Print whether there is an associated value with key 478-0-61159-424-8 or not
-    //Solution1
-    // for(let key in map2){
-    //     if (key === "478-0-61159-424-8") {
-    //         console.log(`There is value with key:478-0-61159-424-8`);
-    //     } else if (key != "478-0-61159-424-8") {
-    //         console.log(`Required key is not found`);
-    //     } else {
-    //         console.log(`error`);
-    //     }
-    // }
-    if (map2.hasOwnProperty("478-0-61159-424-8") === true) {
-        console.log(`There is value with key:478-0-61159-424-8`);
-    } else if (map2.hasOwnProperty("478-0-61159-424-8") === false) {
-        console.log(`Required key is not found`);
-    } else {
-        console.log(`error`)
-    }
-    //Print the value associated with key 978-1-60309-453-5
-    console.log(map2["978-1-60309-453-5"]);
-    console.log(map2["978-60309-453-5"])
+//Solution1
+// for(let key in map2){
+//     if (key === "478-0-61159-424-8") {
+//         console.log(`There is value with key:478-0-61159-424-8`);
+//     } else if (key != "478-0-61159-424-8") {
+//         console.log(`Required key is not found`);
+//     } else {
+//         console.log(`error`);
+//     }
+// }
+if (map2.hasOwnProperty("478-0-61159-424-8") === true) {
+    console.log(`There is value with key:478-0-61159-424-8`);
+} else if (map2.hasOwnProperty("478-0-61159-424-8") === false) {
+    console.log(`Required key is not found`);
+} else {
+    console.log(`error`)
+}
+//Print the value associated with key 978-1-60309-453-5
+console.log(map2["978-1-60309-453-5"]);
+console.log(map2["978-60309-453-5"])
