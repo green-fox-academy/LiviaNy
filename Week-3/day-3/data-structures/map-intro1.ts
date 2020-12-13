1ad20d6c41f18467111d3d1f6066780eff18d61d
LiviaNy <livia.nyitrai@gmail.com>
2020-12-13 20:33:38 +0100

    create-map

---
+++ Week-3/day-3/data-structures/map-intro1.ts
@@ -0,0 +1,11 @@
+`use strict`
+
+//Create an empty map where the keys are integers and the values are characters
+let map:object = {
+    1: `a`,
+    2: `b`,
+    3: `c`
+}
+//Print out whether the map is empty or not
+console.log(map);
+
