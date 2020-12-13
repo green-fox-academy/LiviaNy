759de3a27d9926ec8a41948c4349e7226a70f3c4
LiviaNy <livia.nyitrai@gmail.com>
2020-12-13 20:34:15 +0100

    map-filled

--- Week-3/day-3/data-structures/map-intro1.ts
+++ Week-3/day-3/data-structures/map-intro1.ts
@@ -8,4 +8,18 @@ let map:object = {
 }
 //Print out whether the map is empty or not
 console.log(map);
-
+//Add the following key-value pairs to the map
+// Key	Value
+// 97	a
+// 98	b
+// 99	c
+// 65	A
+// 66	B
+// 67	C
+map[97] = `a`;
+map[98] = `b`;
+map[99] = `c`;
+map[65] = `A`;
+map[66] = `B`;
+map[67] = `C`;
+//Print all the keys
