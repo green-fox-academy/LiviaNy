ce7cd1956d0cffc96b1d15411ac63b9810b75ccb
LiviaNy <livia.nyitrai@gmail.com>
2020-12-14 20:31:40 +0100

    lista-listb

---
+++ Week-3/day-3/data-structures/List-intro2.ts
@@ -0,0 +1,5 @@
+//Create a list ('List A') which contains the following values
+//Apple, Avocado, Blueberries, Durian, Lychee
+let ListA: string[] = [`Apple`, `Avocado`, `Blueberries`, `Durian`, `Lychee`];
+//Create a new list ('List B') with the values of List A
+let ListB: string[] = ListA;
