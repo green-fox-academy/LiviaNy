30ad42610d685436e6937f4aa32013c658ecb855
LiviaNy <livia.nyitrai@gmail.com>
2020-12-14 20:47:11 +0100

    durian

--- Week-3/day-3/data-structures/List-intro2.ts
+++ Week-3/day-3/data-structures/List-intro2.ts
@@ -1,5 +1,18 @@
 //Create a list ('List A') which contains the following values
+
+import { link } from "../simple-replace";
+
 //Apple, Avocado, Blueberries, Durian, Lychee
 let ListA: string[] = [`Apple`, `Avocado`, `Blueberries`, `Durian`, `Lychee`];
 //Create a new list ('List B') with the values of List A
 let ListB: string[] = ListA;
+//Print out whether List A contains Durian or not
+ let include = ListA.includes(`Durian`)
+    if ( include = true) {
+        console.log(`ListA includes Durian`);
+    } else if (include = false) {
+        console.log(`Durian is not within ListA`);
+    } else {
+        console.log(`error`);
+    }
+
