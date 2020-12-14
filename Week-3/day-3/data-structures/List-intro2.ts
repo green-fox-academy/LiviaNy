853588f9388bceba4a13444d1b6ed04a87ec7379
LiviaNy <livia.nyitrai@gmail.com>
2020-12-14 21:05:03 +0100

    Kiwifruit-added

--- Week-3/day-3/data-structures/List-intro2.ts
+++ Week-3/day-3/data-structures/List-intro2.ts
@@ -1,18 +1,31 @@
 //Create a list ('List A') which contains the following values
 
-import { link } from "../simple-replace";
+import { length, link } from "../simple-replace";
 
 //Apple, Avocado, Blueberries, Durian, Lychee
 let ListA: string[] = [`Apple`, `Avocado`, `Blueberries`, `Durian`, `Lychee`];
 //Create a new list ('List B') with the values of List A
-let ListB: string[] = ListA;
+let ListB: string[] = /*ListA*/ [`Apple`, `Avocado`, `Blueberries`, `Durian`, `Lychee`];
 //Print out whether List A contains Durian or not
- let include = ListA.includes(`Durian`)
-    if ( include = true) {
-        console.log(`ListA includes Durian`);
-    } else if (include = false) {
-        console.log(`Durian is not within ListA`);
-    } else {
-        console.log(`error`);
-    }
+let include = ListA.includes(`Durian`)
+if (include = true) {
+    console.log(`ListA includes Durian`);
+} else if (include = false) {
+    console.log(`Durian is not within ListA`);
+} else {
+    console.log(`error`);
+}
+//if (listA.indexOf('Durian') === -1){
+//    console.log(`Durian is not in ListA`);
+//} else {
+//console.log(`Yes, Durian is within ListA`);
+//}
 
+//Remove Durian from List B
+for (let i: number = 0; i < ListB.length; i++) {
+    if (ListB[i] === `Durian`) {
+        ListB.splice(i, 1);
+    }
+}
+//Add Kiwifruit to List A after the 4th element
+ListA.splice(4, 0, `Kiwifruti`);
