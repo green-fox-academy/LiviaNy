1c5c3b78e661cbf84c16191955ea3a8df3bde4cb
LiviaNy <livia.nyitrai@gmail.com>
2020-12-15 22:14:33 +0100

    finishOFDay

--- Week-3/day-3/data-structures/product-database.ts
+++ Week-3/day-3/data-structures/product-database.ts
@@ -17,3 +17,17 @@ let products: object = {
     "Bread": 50,
     "Chicken": 550,
 }
+// Create an application which solves the following problems.
+
+// How much is the fish?
+// What is the most expensive product?
+// What is the average price?
+// How many products' price is below 300?
+// Is there anything we can buy for exactly 125?
+// What is the cheapest product?
+function solvingApp(price) {
+    console.log(`The price is: ` + products[price]);
+    
+}
+
+solvingApp(`Fish`)
