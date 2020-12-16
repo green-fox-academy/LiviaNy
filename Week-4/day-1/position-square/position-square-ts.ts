fc065b43b8c61132075f061df5ec56b154445bdf
LiviaNy <livia.nyitrai@gmail.com>
2020-12-16 20:45:16 +0100

    fst-square

---
+++ Week-4/day-1/position-square/position-square-ts.ts
@@ -0,0 +1,26 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+export {};
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Create a function that draws one square and takes 2 parameters:
+// The x and y coordinates of the square's top left corner
+// and draws a 50x50 square from that point.
+// Draw 3 squares with that function.
+// Avoid code duplication.
+
+function drawSquare(x, y) {
+ctx.beginPath();
+ctx.fillStyle = `purple`
+ctx.moveTo(x, y);
+ctx.lineTo(x + 50, y),
+ctx.lineTo(x + 50, y + 50);
+ctx.lineTo(x, y + 50);
+ctx.closePath();
+ctx.fill()
+}
+
+drawSquare(100, 150);
