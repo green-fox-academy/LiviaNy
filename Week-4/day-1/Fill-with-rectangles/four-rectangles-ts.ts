48fa741556678caf02b596f78ccb1dff08854307
LiviaNy <livia.nyitrai@gmail.com>
2020-12-16 20:23:44 +0100

    foth-rect

--- Week-4/day-1/Fill-with-rectangles/four-rectangles-ts.ts
+++ Week-4/day-1/Fill-with-rectangles/four-rectangles-ts.ts
@@ -2,7 +2,7 @@
 
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
-export {};
+export { };
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
@@ -10,17 +10,17 @@ export {};
 // Avoid code duplication.
 
 ctx.beginPath();
-ctx.fillStyle= `green`
+ctx.fillStyle = `magenta`
 ctx.moveTo(10, 10);
 ctx.lineTo(150, 10),
-ctx.lineTo(150, 100),
-ctx.lineTo(10, 100);
+    ctx.lineTo(150, 100),
+    ctx.lineTo(10, 100);
 ctx.closePath();
 ctx.fill()
 
 
 ctx.beginPath();
-ctx.fillStyle= `pink`
+ctx.fillStyle = `pink`
 ctx.moveTo(350, 10);
 ctx.lineTo(500, 10);
 ctx.lineTo(500, 300);
@@ -29,10 +29,19 @@ ctx.closePath();
 ctx.fill();
 
 ctx.beginPath();
-ctx.fillStyle= `turquoise`
+ctx.fillStyle = `turquoise`
 ctx.moveTo(10, 200);
 ctx.lineTo(160, 200);
 ctx.lineTo(160, 250);
 ctx.lineTo(10, 250);
+ctx.closePath();
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = `cyan`
+ctx.moveTo(170, 10);
+ctx.lineTo(300, 10);
+ctx.lineTo(300, 350);
+ctx.lineTo(170, 350);
 ctx.closePath();
 ctx.fill();
