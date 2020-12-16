b9ca4647f059f4a0a9e38ee2545a20765b756da4
LiviaNy <livia.nyitrai@gmail.com>
2020-12-16 11:01:13 +0100

    line-left-to-right

---
+++ Week-4/day-1/Diagonals/diagonals-ts.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+export {};
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Draw the canvas' diagonals.
+// If it starts from the upper-left corner it should be green, otherwise it should be red.
+
+ctx.beginPath();
+ctx.strokeStyle = `green`;
+ctx.moveTo(0, 0);
+ctx.lineTo(600, 400);
+ctx.stroke();
