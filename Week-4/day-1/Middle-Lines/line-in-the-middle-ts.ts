70acfc7f9cec3cd2a1c0c5c2684bcc3f9b9c4481
LiviaNy <livia.nyitrai@gmail.com>
2020-12-16 09:57:34 +0100

    line-middle

---
+++ Week-4/day-1/Middle-Lines/line-in-the-middle-ts.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+export {};
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// draw a red horizontal line to the canvas' middle.
+// draw a green vertical line to the canvas' middle.
+
+ctx.beginPath();
+ctx.strokeStyle = `red`;
+ctx.moveTo(250, 200);
+ctx.lineTo(350, 200);
+ctx.stroke();
