1f946d1770c8599117c6cccf43c03751196b663b
LiviaNy <livia.nyitrai@gmail.com>
2020-12-17 15:51:15 +0100

    draw-line-funct

---
+++ Week-4/everything-goes-to-center/function-to-center-ts.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+export {};
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Create a function that draws a single line and takes 2 parameters:
+// The x and y coordinates of the line's starting point
+// and draws a line from that point to the center of the canvas.
+// Fill the canvas with lines from the edges, every 20 px, to the center.
+
+function drawLine (x, y){
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(300,200);
+    ctx.stroke();
+}
+
+
