fb032db976a6b004107e1c34920da49245291fbd
LiviaNy <livia.nyitrai@gmail.com>
2020-12-17 22:02:22 +0100

    fixing-ex

--- Week-4/day-1/Middle-Lines/line-in-the-middle-ts.ts
+++ Week-4/day-1/Middle-Lines/line-in-the-middle-ts.ts
@@ -2,23 +2,29 @@
 
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
-export {};
+export { };
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
 // draw a red horizontal line to the canvas' middle.
 // draw a green vertical line to the canvas' middle.
 
-function randomColor() {
-    const colors = ['red', 'green', 'blue', 'black', 'pink', 'magenta', 'orange', 'yellow', 'grey', 'cadetblue', 'salmon', 'teal', 'khaki', 'darkseagreen', 'coral', 'lightseagreen', `gold`, `silver`];
-    const random01 = Math.random();
-    const random1 = (colors.length - 1) * random01;
-    const randINdex = Math.round(random1);
-    return colors[randINdex];
-}
+// function randomColor() {
+//     const colors = ['red', 'green', 'blue', 'black', 'pink', 'magenta', 'orange', 'yellow', 'grey', 'cadetblue', 'salmon', 'teal', 'khaki', 'darkseagreen', 'coral', 'lightseagreen', `gold`, `silver`];
+//     const random01 = Math.random();
+//     const random1 = (colors.length - 1) * random01;
+//     const randINdex = Math.round(random1);
+//     return colors[randINdex];
+// }
 
 ctx.beginPath();
-ctx.strokeStyle = randomColor();
+ctx.strokeStyle = `red`;
 ctx.moveTo(250, 200);
 ctx.lineTo(350, 200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = `green`;
+ctx.moveTo(300, 150);
+ctx.lineTo(300, 250);
 ctx.stroke();
