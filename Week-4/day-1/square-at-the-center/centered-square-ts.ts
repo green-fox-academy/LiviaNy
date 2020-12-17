8b739b0470b571e7b445198972823703f4d8e061
LiviaNy <livia.nyitrai@gmail.com>
2020-12-17 22:07:59 +0100

    fixing-color

--- Week-4/day-1/square-at-the-center/centered-square-ts.ts
+++ Week-4/day-1/square-at-the-center/centered-square-ts.ts
@@ -2,17 +2,17 @@
 
 const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
-export {};
+export { };
 
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
 // Draw a green 10x10 square to the center of the canvas.
 
 ctx.beginPath;
-ctx.strokeStyle = `black`;
+ctx.fillStyle = `green`;
 ctx.moveTo(295, 195);
 ctx.lineTo(295, 205);
 ctx.lineTo(305, 205);
 ctx.lineTo(305, 195);
 ctx.lineTo(295, 195);
-ctx.stroke();
+ctx.fill();
