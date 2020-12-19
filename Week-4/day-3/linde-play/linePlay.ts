3608e47aa57640b6762b890a4f13695f844add23
LiviaNy <livia.nyitrai@gmail.com>
2020-12-19 12:04:38 +0100

    correcting

--- Week-4/day-3/linde-play/linePlay.ts
+++ Week-4/day-3/linde-play/linePlay.ts
@@ -6,14 +6,16 @@ export { };
 
 let distance: number = 20
 
-function drawLines(xy) {
+function drawLines(x, y, x1, y1) {
     ctx.beginPath();
-    ctx.strokeStyle = `purple`
-    ctx.moveTo(0, xy);
-    ctx.lineTo(xy, canvas.height);
+    ctx.strokeStyle = `purple`;
+    ctx.moveTo(x, y);
+    ctx.lineTo(x1, y1);
     ctx.stroke();
 }
 
 for (let i: number = 0; i < canvas.height; i++) {
-    drawLines(distance * i);
+    let x: number = i * canvas.height / (distance - 1);
+    let y: number = i * canvas.width / (distance - 1);
+    drawLines(0, x, y, canvas.height);
 }
