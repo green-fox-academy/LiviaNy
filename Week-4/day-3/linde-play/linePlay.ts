7fac925d52d6214fdb1059c635a2bdc8a955fc8d
LiviaNy <livia.nyitrai@gmail.com>
2020-12-19 12:30:12 +0100

    lineplay

--- Week-4/day-3/linde-play/linePlay.ts
+++ Week-4/day-3/linde-play/linePlay.ts
@@ -16,8 +16,8 @@ function drawLines(x, y, x1, y1) {
 for (let i: number = 0; i < canvas.height; i++) {
     let x: number = i * canvas.width / (distance - 1);
     let y: number = i * canvas.height / (distance - 1);
-    ctx.strokeStyle = `purple`;
-    drawLines(0, y, x, canvas.height);
     ctx.strokeStyle = `green`;
+    drawLines(0, y, x, canvas.height);
+    ctx.strokeStyle = `purple`;
     drawLines(x, 0, canvas.width, y);
 }
