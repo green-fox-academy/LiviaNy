627f3f5a05c5ebcec1ecd81798ae181a3908bda2
LiviaNy <livia.nyitrai@gmail.com>
2021-01-02 13:53:34 +0100

    recheck

---
+++ Week-3/day-1/functions/find-part-of-an-integer.ts
@@ -0,0 +1,15 @@
+'use strict';
+//  Create a function that takes a number and an array of numbers as a parameter
+//  and returns the indices of the numbers of the array which contain the given number
+//  or returns an empty list (if the number is not part of any of the numbers in the array)
+// Example
+//console.log(findMatchingIndexes(1, [1, 11, 34, 52, 61]));
+// should print: `[0, 1, 4]`
+//console.log(findMatchingIndexes(9, [1, 11, 34, 52, 61]));
+// should print: '[]'
+let array1: number[] = [1, 11, 34, 52, 61];
+function findMatchingIndexes(number, array) {
+    for (let i: number = 0; i < arr.length; i++) {
+        let newNumber: number = arr[i];
+    }
+}
