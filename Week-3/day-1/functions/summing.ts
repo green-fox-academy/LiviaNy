0aac4b0bb52394ff6ddd8577d0aa76e32e3dacd5
LiviaNy <livia.nyitrai@gmail.com>
2021-01-05 20:14:53 +0100

    summing-2nd-var

--- Week-3/day-1/functions/summing.ts
+++ Week-3/day-1/functions/summing.ts
@@ -2,9 +2,17 @@
 
 // Write a function called `sum()` that returns the sum of numbers from zero to the given parameter
 
-let length: number = 10;
+let length1: number = 10;
 
-function sum(a: number) {
+// function sum(a: number) {
+//     return ((a * (a + 1) / 2));
+// }
+// console.log(sum(length));
+
+let sum: (a: number) => number = function (
+    a: number
+) {
     return ((a * (a + 1) / 2));
 }
-console.log(sum(length));
+
+console.log(sum(length1));
