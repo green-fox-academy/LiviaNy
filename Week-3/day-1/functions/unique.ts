13b74b4da3ed4b02773523d05f825c7f9adf0fdd
LiviaNy <livia.nyitrai@gmail.com>
2021-01-05 21:59:14 +0100

    fileIO

--- Week-3/day-1/functions/unique.ts
+++ Week-3/day-1/functions/unique.ts
@@ -3,7 +3,7 @@
 //  Create a function that takes a list of numbers as a parameter
 //  and returns a list of numbers where every number is unique (occurs only once)
 let arr: number[] = [1, 11, 34, 11, 52, 61, 1, 34];
-function unique(arr) {
+function unique(arr:any) {
     for (let i: number = 0; i < arr.length; i++) {
         for (let j: number = 0; j < arr.length; j++) {
             if (arr[j] === arr[j + 1]) {
