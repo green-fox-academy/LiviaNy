7267ae161567a8398a20abd0a792e32f306ad824
LiviaNy <livia.nyitrai@gmail.com>
2021-01-05 20:42:34 +0100

    printParams-2nd-var

--- Week-3/day-1/functions/print-params.ts
+++ Week-3/day-1/functions/print-params.ts
@@ -2,8 +2,14 @@
 //    which logs to the console the input parameters
 //    (can have multiple number of arguments)
 
-function printParams(...input: any) {
-    console.log(input);
+// function printParams(...input: any) {
+//     console.log(input);
+// }
+
+let printParams: (...input: any) => void = function (
+    ...input
+) {
+    return console.log(input);
 }
 
 printParams(`i`, `will`, `print`, `411`);
