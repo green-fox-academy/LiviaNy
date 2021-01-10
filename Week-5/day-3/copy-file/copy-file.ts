237a4c68e84658a698e00854a6c7bfffe83ba20c
LiviaNy <livia.nyitrai@gmail.com>
2021-01-10 09:36:18 +0100

    copy-file

--- Week-5/day-3/copy-file/copy-file.ts
+++ Week-5/day-3/copy-file/copy-file.ts
@@ -1,15 +1,17 @@
-import { fileURLToPath } from "url";
-
 `use strict`;
 // Write a function that copies the contents of a file into another
 // It should take the filenames as parameters
 // It should return a boolean that shows if the copy was successful
-let fs = require(`fs`);
+const fs = require(`fs`);
 
-// function copyFile(fileName: string): boolean {
-//     let fileToCopy = fs.readFileSync(fileName, `utf-8`);
-//     fs.writeFileSync(`newFile.txt`, fileToCopy);
-//  return true
-// }
+function copyFile(fileName: string): boolean {
+  try {
+    let fileToCopy = fs.readFileSync(fileName, `utf-8`);
+    fs.writeFileSync(`newFile.txt`, fileToCopy);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
 
-// console.log(copyFile(`copyMe.txt`));
+console.log(copyFile(`copyMe.txt`));
