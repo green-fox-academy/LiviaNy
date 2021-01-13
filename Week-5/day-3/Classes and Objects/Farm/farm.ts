34f34a0a121cab2ad4a3caef24eb4592bfbe6b5f
LiviaNy <livia.nyitrai@gmail.com>
2021-01-13 22:11:19 +0100

    slaughter

--- Week-5/day-3/Classes and Objects/Farm/farm.ts
+++ Week-5/day-3/Classes and Objects/Farm/farm.ts
@@ -5,57 +5,52 @@
 // breed() -> creates a new animal if there's place for it
 // slaughter() -> removes the least hungry animal
 
-import { Animal } from "./../animal/animal"
-let bubbleSort = (inputArr) => {
-    let len = inputArr.length;
-    let swapped;
-    do {
-        swapped = false;
-        for (let i = 0; i < len; i++) {
-            if (inputArr[i] > inputArr[i + 1]) {
-                let tmp = inputArr[i];
-                inputArr[i] = inputArr[i + 1];
-                inputArr[i + 1] = tmp;
-                swapped = true;
-            }
-        }
-    } while (swapped);
-    return inputArr;
-};
+import { Animal } from "./../animal/animal";
 
-
-class Farm{
-    animals:Animal[]
-    slots:number = 3
-    constructor(listOfAnimals:Animal[]){
-        this.animals = listOfAnimals
+class Farm {
+  animals: Animal[];
+  slots: number = 3;
+  constructor(listOfAnimals: Animal[]) {
+    this.animals = listOfAnimals;
+  }
+  breed(newAnimal: Animal): void {
+    if (this.slots > 0) {
+      this.animals.push(newAnimal);
+      this.slots--;
+    } else if (this.slots === 0) {
+      console.log(`The farm is full`);
+    } else {
+      console.log(`error`);
     }
-    breed(newAnimal:Animal):void{
-        if (this.slots > 0){
-        this.animals.push(newAnimal);
-        this.slots--
-        } else if (this.slots === 0) {
-            console.log(`The farm is full`)
-        } else {
-            console.log(`error`)
-        }
+  }
+  slaughter() {
+    let hunger: number[] = [];
+    for (let i: number = 0; i < this.animals.length; i++) {
+      hunger.push(this.animals[i].hunger);
     }
-//     slaughter(){
-//     bubbleSort(this.animals);
-//     console.log(this.animals)
-
-// }
+    let smallestHunger: number = Math.min(...hunger);
+    let indexOfSmallestNumber: number = hunger.indexOf(smallestHunger);
+    this.animals.splice(indexOfSmallestNumber, 1);
+    this.slots++;
+  }
 }
 
-
-
 let theFarm = new Farm([]);
-let cat = new Animal(`Cat`)
+let cat = new Animal(`Cat`);
 let dog = new Animal(`Dog`);
 let bird = new Animal(`Bird`);
 let elephant = new Animal(`Elephant`);
 theFarm.breed(cat);
-theFarm.breed(dog)
+theFarm.breed(dog);
 theFarm.breed(bird);
-theFarm.breed(elephant)
-console.log(theFarm)
+theFarm.breed(elephant);
+console.log(theFarm);
+dog.play();
+dog.play();
+cat.play();
+dog.play();
+cat.play();
+dog.play();
+bird.play();
+theFarm.slaughter();
+console.log(theFarm);
