001f2a8f3b9e72cda7777261d735ccd191532e11
LiviaNy <livia.nyitrai@gmail.com>
2021-01-15 21:12:00 +0100

    bunnies-again

---
+++ Week-6/day-3/bunnies-again/bunnies-again.ts
@@ -0,0 +1,13 @@
+// We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot. Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+function addEars(bunnies: number): number {
+  if (bunnies === 0) {
+    return 0;
+  } else if (bunnies % 2 === 0) {
+    return 3 + addEars(bunnies - 1);
+  } else if (bunnies % 2 === 1) {
+    return 2 + addEars(bunnies - 1);
+  }
+}
+
+console.log(addEars(3));
