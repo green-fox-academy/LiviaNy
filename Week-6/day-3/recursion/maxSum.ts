1ec4b189cbe224798edee1a5b39e8594b4dc5fd4
LiviaNy <livia.nyitrai@gmail.com>
2021-01-16 22:35:30 +0100

    firstTry

---
+++ Week-6/day-3/recursion/maxSum.ts
@@ -0,0 +1,12 @@
+// Find the greatest common divisor of two numbers using recursion.
+// const numberArray: number[] = [1, 2, 3, 4, 5];
+// function maxSum(array: number[]) {
+//   let timer: number = 0;
+//   if (timer === 4) {
+//     return 0;
+//   } else {
+//     Math.max(...array) + maxSum(array.slice(-1));
+//   }
+// }
+
+// console.log(maxSum(numberArray));
