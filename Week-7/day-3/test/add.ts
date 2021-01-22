c5a3b798f37cf65f33bb45c03f099143f324eebe
LiviaNy <livia.nyitrai@gmail.com>
2021-01-22 11:07:54 +0100

    testUnitPractice

--- Week-7/day-3/test/add.ts
+++ Week-7/day-3/test/add.ts
@@ -1,3 +1,5 @@
-module.exports = function (a, b) {
+`use strict`;
+
+export function addTwoNumbers(a: number, b: number): number {
   return a + b;
-};
+}
--- Week-7/day-3/test/main.ts
+++
@@ -1,8 +0,0 @@
-let test = require(`tape`).test;
-let add = require('./add');
-test(`add: add two numbers correctly`, function (t) {
-  let actual = add(1, 2);
-  let expect = 3;
-  t.equal(actual, expect);
-  t.end();
-});
