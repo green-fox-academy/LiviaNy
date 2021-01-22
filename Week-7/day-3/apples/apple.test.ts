318ecf151ddc85944a6198b7ef8e7dbc1ee4fe0a
LiviaNy <livia.nyitrai@gmail.com>
2021-01-22 20:01:56 +0100

    appleTestPassed

---
+++ Week-7/day-3/apples/apple.test.ts
@@ -0,0 +1,11 @@
+import { Apple } from './apples';
+import * as test from 'tape';
+
+test(`getApple(myObject) should return apple`, (t) => {
+  const myObject = new Apple();
+
+  const actual = myObject.getApple(`apple`);
+  const expected = `apple`;
+  t.equal(actual, expected);
+  t.end();
+});
