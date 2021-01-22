302541ebc33679e5e0e92fbd8f4ace3cb4df6cf9
LiviaNy <livia.nyitrai@gmail.com>
2021-01-22 20:02:30 +0100

    appleTestFailed

--- Week-7/day-3/apples/apple.test.ts
+++ Week-7/day-3/apples/apple.test.ts
@@ -1,10 +1,19 @@
 import { Apple } from './apples';
 import * as test from 'tape';
 
+// test(`getApple(myObject) should return apple`, (t) => {
+//   const myObject = new Apple();
+
+//   const actual = myObject.getApple(`apple`);
+//   const expected = `apple`;
+//   t.equal(actual, expected);
+//   t.end();
+// });
+
 test(`getApple(myObject) should return apple`, (t) => {
   const myObject = new Apple();
 
-  const actual = myObject.getApple(`apple`);
+  const actual = myObject.getApple(`green`);
   const expected = `apple`;
   t.equal(actual, expected);
   t.end();
