0f617769219e4afb54533498599be7e88b62221d
LiviaNy <livia.nyitrai@gmail.com>
2021-01-22 20:33:59 +0100

    sumElementsOfList

---
+++ Week-7/day-3/sum/sum.test.ts
@@ -0,0 +1,20 @@
+// Follow these steps:
+// Add a new test case
+// Instantiate your class
+// create a list of integers
+// use the t.equal to test the result of the created sum method
+// Run it
+// Create different tests where you test your method with:
+// an empty list
+// a list that has one element in it
+// a list that has multiple elements in it
+// Run them
+// Fix your code if needed
+
+import {NumbersArray} from "./sum"
+import * as test from "tape"
+
+test(`should return the sum of array elemnts`, t => {
+    const actual = 
+    const expected = 
+})
