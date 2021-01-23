d139de72b7dbcd23ef50c3a186d25bfe90083e19
LiviaNy <livia.nyitrai@gmail.com>
2021-01-23 10:38:00 +0100

    fibonacci

---
+++ Week-7/day-3/fibonacci/fibonacci.ts
@@ -0,0 +1,12 @@
+// Write a function that computes a member of the fibonacci sequence by a given index
+// Create tests for multiple test cases.
+
+function fibonacci(n: number): number {
+  if (n === 2) {
+    return 1;
+  } else if (n === 1) {
+    return 1;
+  } else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
