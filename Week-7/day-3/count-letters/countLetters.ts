2382d9bea32214dfb5a1509073ad2bed072a99ed
LiviaNy <livia.nyitrai@gmail.com>
2021-01-23 10:21:36 +0100

    dictironary

---
+++ Week-7/day-3/count-letters/countLetters.ts
@@ -0,0 +1,17 @@
+// Write a function, that takes a string as an argument and returns a dictionary with all letters in the string as keys, and numbers as values that shows how many occurrences there are.
+// Create a test for that.
+
+function countLetters(text: string): Object {
+  let dictionary: Object = {};
+  let stringArray: string[] = Array.from(text);
+  for (let i: number = 0; i < stringArray.length; i++) {
+    let value: number = 0;
+    for (let j: number = 0; j < stringArray.length; j++) {
+      if (stringArray[j] === stringArray[i]) {
+        value++;
+      }
+    }
+    dictionary[stringArray[i]] = value;
+  }
+  return dictionary;
+}
