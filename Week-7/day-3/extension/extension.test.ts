7e172ba7b756e53d9904d5d97af7a1cf3dcf33b6
LiviaNy <livia.nyitrai@gmail.com>
2021-01-23 13:42:09 +0100

    median test failed

--- Week-7/day-3/extension/extension.test.ts
+++ Week-7/day-3/extension/extension.test.ts
@@ -44,6 +44,11 @@ test('median: five', function (t: any): any {
   t.end();
 });
 
+test(`median:four`, (t) => {
+  t.equal(median([1, 2, 3, 4]), [2, 3]);
+  t.end();
+});
+//------------------------------------------------------------
 test('is vowel: a', function (t: any): any {
   t.ok(isVowel('a'));
   t.end();
