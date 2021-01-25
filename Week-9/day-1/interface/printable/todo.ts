3a8a3c73545d542ead6c1b13c56eef38e36884ba
LiviaNy <livia.nyitrai@gmail.com>
2021-01-25 22:59:51 +0100

    todoPrintable

--- Week-9/day-1/interface/printable/todo.ts
+++ Week-9/day-1/interface/printable/todo.ts
@@ -1,7 +1,18 @@
 `use strict`;
+import { Printable } from './printable';
 
 export class Todo {
   _task: string;
   _priority: string;
   _status: boolean;
+
+  constructor() {
+    this._task = `Buy some milk`;
+    this._priority = `high`;
+    this._status = true;
+  }
+
+  printableFields() {
+    console.log(`Task: ${this._task} | Priority: ${this._priority} | Done: ${this._status}`);
+  }
 }
