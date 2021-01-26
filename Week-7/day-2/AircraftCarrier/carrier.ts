ed4b05bdae322e31878626c5ef7cd15c42cb73ca
LiviaNy <livia.nyitrai@gmail.com>
2021-01-26 16:07:14 +0100

    add aircraft

--- Week-7/day-2/AircraftCarrier/carrier.ts
+++ Week-7/day-2/AircraftCarrier/carrier.ts
@@ -1,1 +1,19 @@
+import { Aircraft } from './Aircraft';
+
 `use strict`;
+
+export class Carrier {
+  protected _aircrafts: Aircraft[];
+  protected _storeOfAmmo: number;
+  protected _initiamAmmo: number;
+  protected _health: number;
+
+  constructor(initialAmmo, health) {
+    this._health = health;
+    this._initiamAmmo = initialAmmo;
+  }
+
+  add(aircraft: Aircraft) {
+    this._aircrafts.push(aircraft);
+  }
+}
