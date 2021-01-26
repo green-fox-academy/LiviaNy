eed407a7a22a6a27ed99d95ad543e42066985641
LiviaNy <livia.nyitrai@gmail.com>
2021-01-26 10:13:22 +0100

    reservation

--- Week-9/day-1/interface/reservation/bookings.ts
+++ Week-9/day-1/interface/reservation/bookings.ts
@@ -1,3 +1,22 @@
-import { Reservationy } from './reservationy';
-
 `use strict`;
+import { Reservationy } from './reservationy';
+const days: string[] = [`Monday`, `Tuesday`, `Wednesday`, `Thusrday`, `Friday`, `Saturday`, `Sunday`];
+
+export class Reservation implements Reservationy {
+  getCodeBooking() {
+    return Math.random().toString(32).substring(2, 10).toLocaleUpperCase();
+  }
+
+  getDowBooking() {
+    let index: number = Math.floor(Math.random() * 6);
+    return days[index].substring(0, 3).toUpperCase();
+  }
+  getReservedBooking() {
+    console.log(`Booking# ${this.getCodeBooking()} for ${this.getDowBooking()}`);
+  }
+}
+
+let newBooking = new Reservation();
+for (let i: number = 0; i < 10; i++) {
+  newBooking.getReservedBooking();
+}
