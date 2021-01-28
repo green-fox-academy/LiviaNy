951caaf61073ce70d32ede92869e844e33cb4481
LiviaNy <livia.nyitrai@gmail.com>
2021-01-28 19:27:39 +0100

    solution with undefined

---
+++ Week-3/day-3/old-exercises/matchmakint.ts
@@ -0,0 +1,22 @@
+'use strict';
+
+import { indexOf, split } from '../strings/simple-replace';
+
+// Write a function that joins two array by matching one girl with one boy in a new array
+// If someone has no pair, he/she should be the element of the array too
+// Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
+
+let girls: string[] = ['Eve', 'Ashley', 'Claire', 'Kat', 'Jane'];
+let boys: string[] = ['Joe', 'Fred', 'Tom', 'Todd', 'Neef', 'Jeff'];
+
+function makingMatches(array1: string[], array2: string[]) {
+  let matchedList: string[] = [];
+  for (let i: number = 0; i < (array2.length || array1.length); i++) {
+    matchedList.push(array1[i]);
+    matchedList.push(array2[i]);
+  }
+  return matchedList;
+}
+console.log(makingMatches(girls, boys));
+
+export = makingMatches;
