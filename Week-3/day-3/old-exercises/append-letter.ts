7788d94940fabc93f4d07ed8acd2216c0ed45e86
LiviaNy <livia.nyitrai@gmail.com>
2021-01-28 19:46:46 +0100

    solution with for loop

---
+++ Week-3/day-3/old-exercises/append-letter.ts
@@ -0,0 +1,17 @@
+'use strict';
+// Create a function called "appendA()" that adds "a" to every string in the far list.
+// The parameter should be a list.
+
+let far: string[] = ['bo', 'anacond', 'koal', 'pand', 'zebr'];
+
+console.log(appendA(far));
+
+// The output should be: 'boa', 'anaconda', 'koala', 'panda', 'zebra'
+function appendA(inputArray: string[]) {
+  let newArray: string[] = [];
+  for (let i: number = 0; i < inputArray.length; i++) {
+    newArray.push(inputArray[i] + `a`);
+  }
+  return newArray;
+}
+export = appendA;
