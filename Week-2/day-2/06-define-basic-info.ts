153e0af2c793b702858d9c5fdb1d80935ddb8010
LiviaNy <livia.nyitrai@gmail.com>
2021-02-09 19:57:39 +0100

    basic info

--- Week-2/day-2/06-define-basic-info.ts
+++ Week-2/day-2/06-define-basic-info.ts
@@ -9,9 +9,9 @@
 let myName: string = `Livia Nyitrai`;
 let myAge: number = 29;
 let myHeightInMetre: number = 1.71;
-let Married: boolean = false;
+let married: boolean = false;
 
 console.log(`My name is: ` + myName);
 console.log(`My age is: ` + myAge);
 console.log(`My height is: ` + myHeightInMetre + ` m`);
-console.log(`I am married: ` + Married);
+console.log(`I am married: ` + married);
