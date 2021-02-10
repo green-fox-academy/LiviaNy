9713dda70659310b81c590153caa4573a01a0b26
LiviaNy <livia.nyitrai@gmail.com>
2021-02-10 11:49:51 +0100

    rename

--- Week-3/day-3/strings/06-he-will-never.ts
+++ Week-3/day-3/strings/06-he-will-never.ts
@@ -7,18 +7,16 @@ let out: string = '';
 let notSoCrypticMessage: number[] = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11];
 
 let hashmap = {
-    7: 'run around and desert you',
-    50: 'tell a lie and hurt you ',
-    49: 'make you cry, ',
-    2: 'let you down',
-    12: 'give you up, ',
-    1: 'Never gonna ',
-    11: '\n',
-    3: 'say goodbye ',
+  7: 'run around and desert you',
+  50: 'tell a lie and hurt you ',
+  49: 'make you cry, ',
+  2: 'let you down',
+  12: 'give you up, ',
+  1: 'Never gonna ',
+  11: '\n',
+  3: 'say goodbye ',
 };
 
 notSoCrypticMessage.forEach(function (value) {
-    console.log(hashmap[value]);
-})
-
-
+  console.log(hashmap[value]);
+});
