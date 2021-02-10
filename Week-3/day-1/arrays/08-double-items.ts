cb724deea89b7d140f2ba0f39c8f8b0c312dadbd
LiviaNy <livia.nyitrai@gmail.com>
2021-02-10 10:29:59 +0100

    correcting double elements

--- Week-3/day-1/arrays/08-double-items.ts
+++ Week-3/day-1/arrays/08-double-items.ts
@@ -1,3 +1,5 @@
+`use strict`;
+
 // - Create an array variable named `drinks` with the following content:
 //   `['Gin', 'Whiskey', 'Wine', 'Beer']`
 // - Double all the strings in the array, use a built in array method instead of
@@ -6,4 +8,4 @@
 
 let drinks: string[] = [`Gin`, `Whiskey`, `Wine`, `Beer`];
 
-drinks.forEach(Element => console.log(Element + Element));
+console.log(drinks.map((element) => element + element));
