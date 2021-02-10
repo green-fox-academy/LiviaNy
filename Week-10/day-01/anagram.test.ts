8fed8246ecd6a47f466c0d707885b4e78f89d78f
LiviaNy <livia.nyitrai@gmail.com>
2021-02-10 20:06:57 +0100

    second test faild

---
+++ Week-10/day-01/anagram.test.ts
@@ -0,0 +1,18 @@
+import * as test from 'tape';
+import { anagram } from './anagram';
+
+test(``, (t) => {
+  const actual = anagram(`alma`, `repa`);
+  const expected = false;
+
+  t.equal(actual, expected);
+  t.end();
+});
+
+test(``, (t) => {
+  const actual = anagram(`alma`, `lama`);
+  const expected = true;
+
+  t.equal(actual, expected);
+  t.end();
+});
---
+++ Week-10/day-01/anagram.ts
@@ -0,0 +1,5 @@
+export function anagram(string1: string, string2: string): boolean {
+  if (string1 !== string2) {
+    return false;
+  }
+}
