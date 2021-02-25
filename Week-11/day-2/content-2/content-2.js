618a4299cb70dc3e98c426e2dc81219958000720
LiviaNy <livia.nyitrai@gmail.com>
2021-02-25 21:36:33 +0100

    background color changed(2 options)

--- Week-11/day-2/content-2/content-2.js
+++ Week-11/day-2/content-2/content-2.js
@@ -7,5 +7,10 @@ for (let i = 0; i < items.length; i++){
     items[i].innerText = newItems[i]
 }
 
-//       2) change the <ul> element's background color to 'limegreen'
+//2) change the <ul> element's background color to 'limegreen'
+const limegreen = document.querySelector(`ul`)
+
+limegreen.style.backgroundColor = `limegreen`
 //         - use css class to change the color instead of the style property
+
+limegreen.setAttribute(`style`, `background-color: lightgreen`)
