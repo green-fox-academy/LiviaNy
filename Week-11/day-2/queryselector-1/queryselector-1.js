06f3def387f274931c1444200eb244937eb74462
LiviaNy <livia.nyitrai@gmail.com>
2021-02-25 17:06:11 +0100

    king and businessLamp variables added

---
+++ Week-11/day-2/queryselector-1/queryselector-1.js
@@ -0,0 +1,18 @@
+`use strict`
+
+//  <script>
+// 1. store the element that says 'The King' in the 'king' variable.
+// console.log it.
+const king = document.getElementById(`b325`)
+console.log(king)
+// 2. store 'The Businessman' and 'The Lamplighter'
+// in the 'businessLamp' variable.
+const businessLamp = [document.querySelector(`p`), document.querySelector(`div.asteroid.b329.big`)]
+// console.log each of them.
+// 3. store 'The King' and 'The Conceited Man'
+// in the 'conceitedKing' variable.
+// alert them one by one.
+// 4. store 'The King', 'The Conceited Man' and 'The Lamplighter'
+// in the 'noBusiness' variable.
+// console.log each of them.
+// </script> */}
