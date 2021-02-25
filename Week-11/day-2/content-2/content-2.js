605bee8e82f1088e173a93e85c3a6f11f2e08965
LiviaNy <livia.nyitrai@gmail.com>
2021-02-25 21:21:18 +0100

    change items texts

---
+++ Week-11/day-2/content-2/content-2.js
@@ -0,0 +1,11 @@
+// 1) replace the list items' content with items from this list:
+//       ['apple', 'banana', 'cat', 'dog']
+
+const items = document.querySelectorAll(`li`)
+const newItems = [`apple`, `banana`, `cat`, `dog`]
+for (let i = 0; i < items.length; i++){
+    items[i].innerText = newItems[i]
+}
+
+//       2) change the <ul> element's background color to 'limegreen'
+//         - use css class to change the color instead of the style property
