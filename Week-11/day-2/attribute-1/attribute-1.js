5663dfece77e31bda0ba0c058e326ffd46b7c0a6
LiviaNy <livia.nyitrai@gmail.com>
2021-02-25 21:48:54 +0100

    favourite animas added

---
+++ Week-11/day-2/attribute-1/attribute-1.js
@@ -0,0 +1,9 @@
+// Write the image's url to the console.
+const image = document.querySelector(`img`)
+console.log(image)
+//       Replace the image with a picture of your favorite animal.
+image.setAttribute(`src` , `https://safety4sea.com/wp-content/uploads/2016/11/green-mamba.jpg`)
+//       Make the link point to the Green Fox Academy website.
+//       Disable the second button.
+//       Replace its text with 'Don't click me!'.
+
