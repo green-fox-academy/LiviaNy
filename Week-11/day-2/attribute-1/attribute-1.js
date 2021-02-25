843b0355c9cab56d5b22610465e0c760fa13a927
LiviaNy <livia.nyitrai@gmail.com>
2021-02-25 21:53:25 +0100

    link added to best website

--- Week-11/day-2/attribute-1/attribute-1.js
+++ Week-11/day-2/attribute-1/attribute-1.js
@@ -4,6 +4,9 @@ console.log(image)
 //       Replace the image with a picture of your favorite animal.
 image.setAttribute(`src` , `https://safety4sea.com/wp-content/uploads/2016/11/green-mamba.jpg`)
 //       Make the link point to the Green Fox Academy website.
+const link = document.querySelector(`a`)
+link.setAttribute(`href`, `https://www.greenfoxacademy.com/`)
 //       Disable the second button.
+
 //       Replace its text with 'Don't click me!'.
 
