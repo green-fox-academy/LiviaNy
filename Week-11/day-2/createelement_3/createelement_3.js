38df98077238c31074c25ed8f1ca275b9dd56fe9
LiviaNy <livia.nyitrai@gmail.com>
2021-03-04 21:40:44 +0100

    articles added

--- Week-11/day-2/createelement_3/createelement_3.js
+++ Week-11/day-2/createelement_3/createelement_3.js
@@ -1,3 +1,5 @@
+const { runInNewContext } = require("vm");
+
 const kids = [
     { 'petName': 'Wattled crane', 'owner': 'Bryant' },
     { 'petName': 'Devil, tasmanian', 'owner': 'Alejandro' },
@@ -11,8 +13,14 @@ const kids = [
     { 'petName': 'Carpet snake', 'owner': 'Veda' },
     { 'petName': 'Lesser mouse lemur', 'owner': 'Isidor' },
   ];
-  1) Create an <article> element for each kid
-  2) Create a <h3> and a <p> element for each article and append them as a child to the <article>
-      - The H3 should contain the owner's name
-      - The p should contain the pet's name
-  3) Add the article to the pets main.
+//   1) Create an <article> element for each kid
+const main = document.querySelector(`main`)
+for(let i =0; i< kids.length;i++){
+    const newArticle = document.createElement(`article`)
+    main.appendChild(newArticle)
+}
+
+//   2) Create a <h3> and a <p> element for each article and append them as a child to the <article>
+//       - The H3 should contain the owner's name
+//       - The p should contain the pet's name
+//   3) Add the article to the pets main.
