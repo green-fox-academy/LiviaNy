70167e95dcbf851b6a6a4f239960d38bca3ebeb4
LiviaNy <livia.nyitrai@gmail.com>
2021-03-04 21:49:17 +0100

    create h 3 and p

--- Week-11/day-2/createelement_3/createelement_3.js
+++ Week-11/day-2/createelement_3/createelement_3.js
@@ -14,13 +14,22 @@ const kids = [
     { 'petName': 'Lesser mouse lemur', 'owner': 'Isidor' },
   ];
 //   1) Create an <article> element for each kid
-const main = document.querySelector(`main`)
+const main = document.querySelector(`main`);
 for(let i =0; i< kids.length;i++){
-    const newArticle = document.createElement(`article`)
-    main.appendChild(newArticle)
+    const newArticle = document.createElement(`article`);
+    main.appendChild(newArticle);
 }
 
 //   2) Create a <h3> and a <p> element for each article and append them as a child to the <article>
 //       - The H3 should contain the owner's name
 //       - The p should contain the pet's name
+for(let i =0; i< kids.length;i++){
+const newH = document.createElement(`h3`);
+const newP = document.createElement(`p`);
+newH.textContent = kids[i].owner;
+newP.textContent = kids[i].petName;
+newArticle[i].appendChild(newH);
+newArticle[i].appendChild(newP);
+}
+
 //   3) Add the article to the pets main.
