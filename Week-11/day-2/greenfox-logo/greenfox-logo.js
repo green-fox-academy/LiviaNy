2bbaa8b24a58a4c4710c75ba4b4364b1f6dadd0c
LiviaNy <livia.nyitrai@gmail.com>
2021-03-06 15:38:17 +0100

    right eye colored

--- Week-11/day-2/greenfox-logo/greenfox-logo.js
+++ Week-11/day-2/greenfox-logo/greenfox-logo.js
@@ -6,3 +6,4 @@ const leftEye = document.getElementById(`left-eye`);
 console.log(leftEye);
 const rightEye = document.getElementById(`right-eye`);
 leftEye.setAttribute(`fill`, `tomato`);
+rightEye.setAttribute(`fill`, `tomato`);
