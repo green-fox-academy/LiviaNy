1fb2556a5820d2bf1cb90012877d590e77ff0c21
LiviaNy <livia.nyitrai@gmail.com>
2021-03-06 15:26:44 +0100

    rectangle selected

---
+++ Week-11/day-2/svg-1/svg1.js
@@ -0,0 +1,6 @@
+// 1) Select the rectangle by the ID: "very-rectangle"
+
+const rectangle = document.getElementById(`very-rectangle`);
+
+// 2) Change the rectangle's position to x:50, y:50
+// 3) Change its fill to tomato
