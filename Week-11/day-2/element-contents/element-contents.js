f326d2eb84ed148378843a2935908cdc0e2dc017
LiviaNy <livia.nyitrai@gmail.com>
2021-03-06 09:22:00 +0100

    create elements example

---
+++ Week-11/day-2/element-contents/element-contents.js
@@ -0,0 +1,5 @@
+const asteroid = document.querySelector('.asteroid');
+
+console.log(asteroid.innerHTML);
+console.log(asteroid.textContent);
+asteroid.innerHTML = 'This is your <strong>new content!</strong>';
