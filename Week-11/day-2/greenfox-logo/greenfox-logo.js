44b8c12d89f375f9eb2ea46f59afd445fd97aec9
LiviaNy <livia.nyitrai@gmail.com>
2021-03-06 15:37:46 +0100

    left eye colored

---
+++ Week-11/day-2/greenfox-logo/greenfox-logo.js
@@ -0,0 +1,8 @@
+//   Select left and right eyes
+//   They have #left-eye and #right-eye
+//   turn their color (fill) to tomato
+
+const leftEye = document.getElementById(`left-eye`);
+console.log(leftEye);
+const rightEye = document.getElementById(`right-eye`);
+leftEye.setAttribute(`fill`, `tomato`);
