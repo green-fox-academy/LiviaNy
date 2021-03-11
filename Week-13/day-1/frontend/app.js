db5d27573fa80bb30d07683b0c1831fb6130ccc1
LiviaNy <livia.nyitrai@gmail.com>
2021-03-11 20:26:16 +0100

    doubling param done

--- Week-13/day-1/frontend/app.js
+++ Week-13/day-1/frontend/app.js
@@ -12,6 +12,24 @@ app.get(`/`, (req, res) => {
   res.sendFile(path.join(__dirname, `index.html`));
 });
 
+app.get(`/doubling/`, (req, res) => {
+  const { input } = req.query;
+
+  if (input === undefined) {
+    const resObject = {
+      error: `Please provide an input!`,
+    };
+    res.json(resObject);
+  } else {
+    const intInput = parseInt(input);
+    const inputDouble = intInput * 2;
+    res.json({
+      received: intInput,
+      result: inputDouble,
+    });
+  }
+});
+
 app.listen(PORT, () => {
   `Server now listens to PORT 3000`;
 });
