d99608979865348cbbfdce76fc6e08eea755e035
LiviaNy <livia.nyitrai@gmail.com>
2021-03-11 19:51:29 +0100

    other solution

--- Week-12/day-2/server.js
+++ Week-12/day-2/server.js
@@ -1,13 +1,15 @@
 `use strict`;
-const express = require('express');
+
+const express = require(`express`);
 const app = express();
-const port = 3000;
-const path = require('path');
+const path = require(`path`);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + `/index.html`);
+app.use(`/`, function (req, res) {
+  res.sendFile(path.join(__dirname, `index.html`));
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.use(`/hu`, function (req, res) {
+  res.sendFile(path.join(__dirname, `index.hu.html`));
 });
+
+app.listen(3000);
