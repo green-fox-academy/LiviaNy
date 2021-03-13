c99d423cb5d3d1c6a4f9a8d9c7931ada0f268a71
LiviaNy <livia.nyitrai@gmail.com>
2021-03-13 21:10:14 +0100

    prime squares done

--- Week-13/day-3/callback/prime-squares/index.js
+++ Week-13/day-3/callback/prime-squares/index.js
@@ -1,9 +1,39 @@
 `use strict`;
-
+// generate 100 divs to the <section> element and add index numbers to it as the element's textContent
 const section = document.querySelector(`section`);
-for (i = 0; i < 100; i++) {
+for (i = 2; i <= 100; i++) {
   let divElement = document.createElement(`div`);
-  divElement.innerText = i + 1;
+  divElement.innerText = i;
   section.appendChild(divElement);
 }
-console.log(section);
+
+// Create a function that adds a 'not-prime' class to a div if it's not a prime and 'prime' if it is
+const divArray = document.querySelectorAll(`div`);
+
+function isPrime(div) {
+  const number = parseInt(div.innerText);
+  if (number === 1) {
+    return div.setAttribute(`class`, `not-prime`);
+  } else if (number === 2) {
+    return div.setAttribute(`class`, `prime`);
+  }
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return div.setAttribute(`class`, `prime`);
+    }
+  }
+  return div.setAttribute(`class`, `not-prime`);
+}
+
+// Create a timer that keeps calling the prime validator function until it reaches the last element
+// the timer should fire every 100ms
+// the timer should stop when there are no more elements left to be colored
+
+let index = 0;
+let interval = setInterval(() => {
+  isPrime(divArray[index]);
+  index++;
+  if (index >= divArray.length) {
+    clearInterval(interval);
+  }
+}, 1000);
