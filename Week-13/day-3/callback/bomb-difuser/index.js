79996f538cd56ac56b6ea517917361b3453de10a
LiviaNy <livia.nyitrai@gmail.com>
2021-03-18 19:01:45 +0100

    bomb explodes in 10 sec

---
+++ Week-13/day-3/callback/bomb-difuser/index.js
@@ -0,0 +1,31 @@
+`use strict`;
+// Create a timeout that will set the display to "Bomb exploded" in 10 seconds
+// If you click on the button set the display to "Bomb defused" and stop the timeout
+// Extra: create an interval which will update the remaining seconds in the display
+const timer = document.querySelector(`.display`);
+const bomb = document.querySelector(`button`);
+
+function bombDeffuser() {
+  setTimeout(() => {
+    bomb.removeEventListener(`click`);
+  });
+}
+
+// const timeCounter = setInterval(() => {
+//     const timeCounter = setInterval(() => {
+//       let counter = parseInt(timer.innerText);
+//       let setTime = setInterval(() => {
+//         if (counter === 0) {
+//           timer.innerText(`Bomb exploded`);
+//           clearInterval(timeCounter);
+//         } else {
+//           document.querySelector(`.display`).textContent = counter - 1;
+// }}),1000}
+const timeCounter = setInterval(() => {
+  let counter = parseInt(timer.innerHTML);
+  timer.textContent = counter - 1;
+  if (counter === 1) {
+    bomb.textContent = `Bomb exploded.`;
+    clearInterval(timeCounter);
+  }
+}, 1000);
