202ba6e56c8ca83abfa2be343181937906667d6c
LiviaNy <livia.nyitrai@gmail.com>
2021-03-20 20:38:53 +0100

    star wars characters

--- Week-13/day-3/XHR/app.js
+++ Week-13/day-3/XHR/app.js
@@ -2,13 +2,32 @@
 
 const button = document.querySelector(`#button`);
 const input = document.querySelector(`#input`);
-const list = document.querySelector(`#list`);
+const left = document.querySelector(`#list-left`);
+const right = document.querySelector(`#list-right`);
+const movies = document.querySelector(`#movies`);
+let character = ``;
 
 button.onclick = () => {
   const xhr = new XMLHttpRequest();
-  xhr.open(`GET`, `https://swapi.dev/api/people/?search=${search.value}`, true);
-  xhr.onload = () => {
-    const data = JSON.parse(xhr.responseText);
-  };
+  xhr.open(`GET`, `https://swapi.dev/api/people/?search=${input.value}`, true);
   xhr.send();
+  xhr.onload = () => {
+    const response = JSON.parse(xhr.response);
+    clearName();
+    for (let i = 0; i < response.results.length; i++) {
+      getName(response.results[i].name);
+      console.log(response.results[i]);
+    }
+  };
 };
+
+function getName(name) {
+  let searchName = document.createElement(`li`);
+  searchName.setAttribute(`class`, `left`);
+  searchName.textContent = name;
+  left.appendChild(searchName);
+}
+
+function clearName() {
+  left.textContent = ``;
+}
