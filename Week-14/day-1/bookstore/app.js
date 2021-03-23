40b225240ff7dad28a7bec9844abf59098429a57
LiviaNy <livia.nyitrai@gmail.com>
2021-03-23 21:35:11 +0100

    bookshop done

--- Week-14/day-1/bookstore/app.js
+++ Week-14/day-1/bookstore/app.js
@@ -30,14 +30,53 @@ app.get(`/`, (req, res) => {
   res.send(`Server works perfectly`);
 });
 
-app.get(`/titles`, (req, res) => {
-  conn.query(`SELECT book_name FROM book_mast;`, (err, result) => {
-    if (err) {
-      res.status(500).json({ error: `database error` });
-      return;
+app.get(`/books`, (req, res) => {
+  conn.query(
+    'SELECT book_mast.book_id, book_mast.book_name, author.aut_name, category.cate_descrip, publisher.pub_name, book_mast.book_price FROM book_mast JOIN author ON author.aut_id=book_mast.aut_id JOIN category ON category.cate_id=book_mast.cate_id JOIN publisher ON publisher.pub_id=book_mast.pub_id;',
+    (err, result) => {
+      if (err) {
+        res.status(500).json({ error: `database error` });
+        return;
+      }
+      let filtered = {};
+      if (Object.keys(req.query).length === 0) {
+        res.send(result);
+        return;
+      }
+
+      Object.keys(req.query).forEach((filter) => {
+        if (filter === 'category') {
+          filtered = result.filter((item) => item.cate_descrip.toLowerCase() === req.query.category.toLowerCase());
+        }
+        if (filter === 'author') {
+          filtered = result.filter(
+            (item) =>
+              item.aut_name.toLowerCase().replace(/\s/g, '') === req.query.author.toLowerCase().replace(/\s/g, '')
+          );
+        }
+        if (filter === 'title') {
+          filtered = result.filter(
+            (item) =>
+              item.book_name.toLowerCase().replace(/\s/g, '') === req.query.title.toLowerCase().replace(/\s/g, '')
+          );
+        }
+        if (filter === 'publisher') {
+          filtered = result.filter(
+            (item) =>
+              item.pub_name.toLowerCase().replace(/\s/g, '') === req.query.publisher.toLowerCase().replace(/\s/g, '')
+          );
+        }
+        if (filter === 'plt') {
+          filtered = result.filter((item) => Number(item.book_price) <= Number(req.query.plt));
+        }
+        if (filter === 'pgt') {
+          filtered = result.filter((item) => Number(item.book_price) >= Number(req.query.pgt));
+        }
+      });
+
+      res.send(filtered);
     }
-    res.json(result);
-  });
+  );
 });
 
 app.listen(PORT, () => {
--- Week-14/day-1/bookstore/public/script.js
+++ Week-14/day-1/bookstore/public/script.js
@@ -1,24 +1,40 @@
 `use strict`;
 
-const url = `localhost:3000`;
+const url = `http://localhost:3000`;
+let params = window.location.search.substr(1);
 
 const xhr = new XMLHttpRequest();
 xhr.onload = () => {
   const titles = JSON.parse(xhr.response);
-  console.log(titles);
-  addBookTitles(titles.book_name);
+  addBookTitles(titles);
 };
-xhr.open(`GET`, `${url}/titles`, true);
+xhr.open(`GET`, `${url}/books?${params}`, true);
 xhr.send();
 
-function addBookTitles(bookTitles) {
+function addBookTitles(bookInfo) {
   const tbody = document.querySelector(`tbody`);
-  books.forEach((element) => {
+  bookInfo.forEach((element) => {
     const tr = document.createElement(`tr`);
     tbody.appendChild(tr);
     const title = document.createElement(`th`);
     title.setAttribute(`class`, `title`);
-    title.textContent = element;
+    title.textContent = element.book_name;
     tr.appendChild(title);
+    const author = document.createElement(`th`);
+    author.setAttribute(`class`, `author`);
+    author.textContent = element.aut_name;
+    tr.appendChild(author);
+    const category = document.createElement(`th`);
+    category.setAttribute(`class`, `category`);
+    category.textContent = element.cate_descrip;
+    tr.appendChild(category);
+    const publisher = document.createElement(`th`);
+    publisher.setAttribute(`class`, `publisher`);
+    publisher.textContent = element.pub_name;
+    tr.appendChild(publisher);
+    const price = document.createElement(`th`);
+    price.setAttribute(`class`, `price`);
+    price.textContent = element.book_price;
+    tr.appendChild(price);
   });
 }
