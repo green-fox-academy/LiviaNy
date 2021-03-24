430c467e67bf94ab4e1c52ba1d193ccb4a8500de
LiviaNy <livia.nyitrai@gmail.com>
2021-03-24 10:14:25 +0100

    post request works

--- Week-14/day-3-reddit/server.js
+++ Week-14/day-3-reddit/server.js
@@ -1,15 +1,42 @@
 `use strict`;
 
 import express from 'express';
-// import mysql from "mysql";
+import mysql from 'mysql';
 
 const app = express();
 const PORT = 3000;
+
+let conn = mysql.createConnection({
+  host: `localhost`,
+  user: `root`,
+  password: `password`,
+  database: `reddit`,
+  insecureAuth: `true`,
+});
+
+conn.connect((err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log(`Connected to mysql`);
+  }
+});
 
 app.use(express.static(`public`));
 
 app.get(`/hello`, (req, res) => {
   res.send(`Hello world`);
+});
+
+app.get(`/post`, (req, res) => {
+  res.set(`Content-type`, `application/json`);
+  conn.query(`SELECT * FROM post`, (err, result) => {
+    if (err) {
+      res.status(500).json({ errror: `database error` });
+      return;
+    }
+    return res.json({ post: result });
+  });
 });
 
 app.listen(PORT, () => {
