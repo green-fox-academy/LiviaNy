71d20dab2b837411a50b1f5635d51d183c14cc25
LiviaNy <livia.nyitrai@gmail.com>
2021-03-27 14:05:02 +0100

    bubbling

--- Week-15/day-2/bubbling/script.js
+++ Week-15/day-2/bubbling/script.js
@@ -7,3 +7,26 @@
 //    - attach only two event listeners to the nav element
 //      - one for navigation
 //      - one for zooming
+
+const buttons = document.querySelector('nav');
+const image = document.querySelector(`.img-inspector`);
+
+let positionX = 0;
+let positionY = 0;
+let zoom = 200;
+buttons.addEventListener(`click`, (event) => {
+  const direction = event.target.dataset.direction;
+  if (direction === `up`) {
+    image.style.backgroundPositionY = `${(positionY -= 10)}px`;
+  } else if (direction === `down`) {
+    image.style.backgroundPositionY = `${(positionY += 10)}px`;
+  } else if (direction === `left`) {
+    image.style.backgroundPositionX = `${(positionX -= 10)}px`;
+  } else if (direction === `right`) {
+    image.style.backgroundPositionX = `${(positionX += 10)}px`;
+  } else if (direction === `in`) {
+    image.style.backgroundSize = `${(zoom += 20)}%`;
+  } else if (direction === `out`) {
+    image.style.backgroundSize = `${(zoom -= 20)}%`;
+  }
+});
