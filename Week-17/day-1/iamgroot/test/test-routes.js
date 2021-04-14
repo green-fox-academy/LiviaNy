4d0160896933ba8a316538805b377a7be17cc6c2
LiviaNy <livia.nyitrai@gmail.com>
2021-04-14 21:11:08 +0200

    send error bad request

--- Week-17/day-1/iamgroot/test/test-routes.js
+++ Week-17/day-1/iamgroot/test/test-routes.js
@@ -7,7 +7,7 @@ const expect = chai.expect;
 
 describe('groot endpoints', () => {
   // TODO: implement it
-  it('GET /groot/?message=valami should return 200 sttatus code', (done) => {
+  it('GET /groot/?message=valami should return 200 status code', (done) => {
     request(app)
       .get('/groot/?message=valami')
       .expect(200)
@@ -16,6 +16,17 @@ describe('groot endpoints', () => {
         expect(res.status).to.equal(200);
         expect(res.body.received).to.equal(`valami`);
         expect(res.body.translated).to.equal(`I am Groot!`);
+        done();
+      });
+  });
+
+  it('GET /groot/ should return 400 error status is not ok', (done) => {
+    request(app)
+      .get('/groot/')
+      .expect(400)
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res.body.error).to.equal('I am Groot');
         done();
       });
   });
