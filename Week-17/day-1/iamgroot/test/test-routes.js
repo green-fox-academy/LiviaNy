3b9953fea7ac059d626d5074c83eb20be9df8839
LiviaNy <livia.nyitrai@gmail.com>
2021-04-14 21:01:13 +0200

    status is correct

--- Week-17/day-1/iamgroot/test/test-routes.js
+++ Week-17/day-1/iamgroot/test/test-routes.js
@@ -1,9 +1,20 @@
 'use strict';
 
-const test = require('tape');
-const request = require('supertest');
-const app = require('../routes');
-​
-test('groot endpoint', (t) => {
+import request from 'supertest';
+import chai from 'chai';
+import { app } from '../routes.js';
+const expect = chai.expect;
+
+describe('groot endpoints', () => {
   // TODO: implement it
+  it('GET /groot/?message=valami should return 200 sttatus code', (done) => {
+    request(app)
+      .get('/groot/?message=valami')
+      .expect(200)
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
 });
