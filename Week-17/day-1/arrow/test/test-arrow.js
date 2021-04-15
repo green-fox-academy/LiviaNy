67e855b8805775c97ceabe4345b577fbec704bc6
LiviaNy <livia.nyitrai@gmail.com>
2021-04-15 09:33:16 +0200

    yondu route added

---
+++ Week-17/day-1/arrow/test/test-arrow.js
@@ -0,0 +1,22 @@
+`use strict`;
+
+import request from 'supertest';
+import chai from 'chai';
+import { app } from '../routes';
+const expect = chai.expect;
+
+describe('yondu endpoints', () => {
+  it('GET /yondu/distance=100.0&time=10.0 should return response object', (done) => {
+    request(app)
+      .get('/yondu/distance=100.0&time=10.0')
+      .expect(200)
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res.status).to.equal(200);
+        expect(res.body.distance).to.equal(100.0);
+        expect(res.body.time).to.equal(10.0);
+        expect(res.body.speed).to.equal(100.0);
+        done();
+      });
+  });
+});
