d5f8cfe1241d9d39bbd873ded684514a158e1d9d
LiviaNy <livia.nyitrai@gmail.com>
2021-04-15 18:43:33 +0200

    test passed

--- Week-17/day-1/arrow/test/test-arrow.js
+++ Week-17/day-1/arrow/test/test-arrow.js
@@ -2,20 +2,20 @@
 
 import request from 'supertest';
 import chai from 'chai';
-import { app } from '../routes';
+import { app } from '../routes.js';
 const expect = chai.expect;
 
 describe('yondu endpoints', () => {
-  it('GET /yondu/distance=100.0&time=10.0 should return response object', (done) => {
+  it('GET /yondu/?distance=100.0&time=10.0 should return response object', (done) => {
     request(app)
-      .get('/yondu/distance=100.0&time=10.0')
+      .get('/yondu/?distance=100.0&time=10.0')
       .expect(200)
       .end((err, res) => {
         expect(err).to.be.null;
         expect(res.status).to.equal(200);
         expect(res.body.distance).to.equal(100.0);
         expect(res.body.time).to.equal(10.0);
-        expect(res.body.speed).to.equal(100.0);
+        expect(res.body.speed).to.equal(10.0);
         done();
       });
   });
