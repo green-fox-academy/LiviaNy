af1cbd4f7e0ce7e9cd65e4a7437216f939f0ae8f
LiviaNy <livia.nyitrai@gmail.com>
2021-04-17 20:08:04 +0200

    drax post request test passed

--- Week-17/day-1/calorie-table/test/test-calorie-tale.js
+++ Week-17/day-1/calorie-table/test/test-calorie-tale.js
@@ -16,4 +16,19 @@ describe('/drax endpoints', () => {
         done();
       });
   });
+
+  it('POST /drax/?name=snail&amount=1&calorie=100 should return status 200', (done) => {
+    request(app)
+      .post('/drax/?name=snail&amount=1&calorie=100')
+      .expect(200)
+      .send({ name: 'snail', amount: '1', calorie: '100' })
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res.status).to.equal(200);
+        expect(res.body[4].name).to.equal('snail');
+        expect(res.body[4].amount).to.equal(`1`);
+        expect(res.body[4].calorie).to.equal(`100`);
+        done();
+      });
+  });
 });
