25fa620357a9953379079eaee7b277033dfdf2ad
LiviaNy <livia.nyitrai@gmail.com>
2021-04-17 17:32:43 +0200

    delete function wokrs

---
+++ Week-17/day-1/calorie-table/routes.js
@@ -0,0 +1,50 @@
+`use strict`;
+
+import express from 'express';
+export const app = express();
+
+let calorieTable = [
+  { name: 'worm', amount: '1', calories: '100' },
+  { name: 'leech', amount: '1', calories: '300' },
+  { name: 'insect', amount: '1', calories: '50' },
+  { name: 'cockroach', amount: '1', calories: '250' },
+];
+
+app.use(express.json());
+
+app.get('/drax', (req, res) => {
+  res.status(200).send(calorieTable);
+});
+
+app.post(`/drax/`, (req, res) => {
+  const { name } = req.body;
+  const { amount } = req.body;
+  const { calorie } = req.body;
+  if (!name || !amount || !calorie) {
+    res.status(400).send({ error: 'Please feed me some data' });
+  } else {
+    addToList(name, amount, calorie);
+    res.status(200).send(calorieTable);
+  }
+});
+
+app.delete('/drax/:index', (req, res) => {
+  const { index } = req.params;
+
+  if (!index) {
+    res.status(400).send({ error: 'Please feed me some data' });
+  } else {
+    deleteList(index - 1);
+    console.log(calorieTable);
+    res.status(200).send(calorieTable);
+  }
+});
+
+function addToList(newName, newAmount, newCalorie) {
+  const newList = { name: newName, amount: newAmount, calorie: newCalorie };
+  calorieTable.push(newList);
+}
+
+function deleteList(index) {
+  calorieTable.splice(index, 1);
+}
