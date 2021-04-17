1ada63d5da1150c28cc9fbafa3d00fbe85dc3626
LiviaNy <livia.nyitrai@gmail.com>
2021-04-17 14:58:07 +0200

    cargofill works

--- Week-17/day-1/cargo/routes.js
+++ Week-17/day-1/cargo/routes.js
@@ -2,7 +2,6 @@
 
 import express from 'express';
 export const app = express();
-const port = 3000;
 
 const actualCargo = {
   caliber25: 0,
@@ -16,11 +15,73 @@ app.get('/rocket', (req, res) => {
   res.status(200).send(ship);
 });
 
-app.get('/rocket/fill', () => {
+app.get('/rocket/fill', (req, res) => {
   const caliber = req.query.caliber;
-  const amount = req.query.amount;
+  const amount = parseInt(req.query.amount);
+  if (!caliber || !amount) {
+    res.status(400).send({ error: 'Please feed me some data' });
+  } else {
+  }
+  console.log(actualCargo);
+  const actualAmmo = actualCargo.caliber25 + actualCargo.caliber30 + actualCargo.caliber50;
+  loadShip(caliber, actualAmmo, amount);
+  res.send({
+    received: caliber,
+    amount: amount,
+    shipStatus: actualCargo.shipstatus,
+    ready: actualCargo.ready,
+  });
+  console.log(actualCargo);
 });
 
-app.listen(port, () => {
-  console.log(`app is running on port ${port}`);
-});
+function loadShip(caliber, actualAmmo, ammoToFill) {
+  const ammo = actualAmmo + ammoToFill;
+  if (caliber === '.25') {
+    if (ammo === 0) {
+      shipStatus('empty', false);
+      actualCargo.caliber25 += ammoToFill;
+    } else if (ammo === 12500) {
+      shipStatus('full', true);
+      actualCargo.caliber25 += ammoToFill;
+    } else if (ammo > 12500) {
+      shipStatus('overloaded', false);
+      actualCargo.caliber25 += ammoToFill;
+    } else {
+      actualCargo.caliber25 += ammoToFill;
+      shipStatus(`${(ammo / 12500) * 100}%`, false);
+    }
+  } else if (caliber === '.30') {
+    if (ammo === 0) {
+      shipStatus('empty', false);
+      actualCargo.caliber30 += ammoToFill;
+    } else if (ammo === 12500) {
+      shipStatus('full', true);
+      actualCargo.caliber30 += ammoToFill;
+    } else if (ammo > 12500) {
+      shipStatus('overloaded', false);
+      actualCargo.caliber30 += ammoToFill;
+    } else {
+      shipStatus(`${(ammo / 12500) * 100}%`, false);
+      actualCargo.caliber30 += ammoToFill;
+    }
+  } else if (caliber === '.50') {
+    if (ammo === 0) {
+      shipStatus('empty', false);
+      actualCargo.caliber50 += ammoToFill;
+    } else if (ammo === 12500) {
+      shipStatus('full', true);
+      actualCargo.caliber50 += ammoToFill;
+    } else if (ammo > 12500) {
+      shipStatus('overloaded', false);
+      actualCargo.caliber50 += ammoToFill;
+    } else {
+      actualCargo.caliber50 += ammoToFill;
+      shipStatus(`${(ammo / 12500) * 100}%`, false);
+    }
+  }
+}
+
+function shipStatus(status, ready) {
+  actualCargo.shipstatus = status;
+  actualCargo.ready = ready;
+}
